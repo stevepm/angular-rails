10c48f05726c6cad69e46db6c8b6167c22409e1c
Steven Magelowitz <steven.magelowitz@gmail.com>
2015-01-31 23:30:16 -0800

    Add canned search functionality to home page

--- app/assets/javascripts/app.js
+++ app/assets/javascripts/app.js
@@ -12,7 +12,41 @@ myApp.config(['$routeProvider', function ($routeProvider) {
         });
 }]);
 
-controllers = angular.module('controllers',[]);
-controllers.controller("RecipesController", ['$scope', function($scope){
+recipes = [
+    {
+        id: 1,
+        name: 'Baked Potato w/ Cheese'
+    },
+    {
+        id: 2,
+        name: 'Garlic Mashed Potatoes'
+    },
+    {
+        id: 3,
+        name: 'Potatoes Au Gratin'
+    },
+    {
+        id: 4,
+        name: 'Baked Brussel Sprouts'
+    }
+];
 
-}]);
+controllers = angular.module('controllers', []);
+controllers.controller("RecipesController",
+    ['$scope', '$routeParams', '$location',
+        function ($scope, $routeParams, $location) {
+            var keywords;
+
+            $scope.search = function (keywords) {
+                return $location.path('/').search('keywords', keywords);
+            };
+
+            if ($routeParams.keywords) {
+                keywords = $routeParams.keywords.toLowerCase();
+                return $scope.recipes = recipes.filter(function (recipe) {
+                    return recipe.name.toLowerCase().indexOf(keywords) !== -1;
+                });
+            } else {
+                $scope.recipes = [];
+            }
+        }]);
